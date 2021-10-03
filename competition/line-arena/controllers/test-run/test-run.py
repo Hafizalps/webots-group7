94b411814175e6061110657124317e49ac7c34f4
Hafizalps <hafizalperspicarhanggasidhi.2019@student.uny.ac.id>
2021-10-04 01:20:59 +0700

    pid control

--- competition/line-arena/controllers/test-run/test-run.py
+++ competition/line-arena/controllers/test-run/test-run.py
@@ -4,26 +4,20 @@ def kalkulasi_motor(signal):
     return (signal/100)*30
 
 nilai_posisi = 0
-
-""""def pid_calc():
-    rata_rata = 0
-    sum_ir = 0
-    lastErrorLF = 0
-    pid_control = 0
-
-    for i in range(6):
-        rata_rata += sensor_ir_val[i] * i * 1000
-        sum_ir += sensor_ir_val[i]
-
-    posisi = rata_rata/sum_ir #Posisi saat ini
-    error = posisi - set_point_lf
-    p = pid_parameter[0] * error
-    d = pid_parameter[1] * (error - lastErrorLF)
-    pid_control = p + d
-    lastErrorLF = error"""
+kecepatan_lambat = 20
+kecepatan_normal = 60
+kecepatan_cepat = 100
+pid_control = 0
 
 def run_robot(robot):
-    timestep = int(robot.getBasicTimeStep())    
+    global kecepatan_lambat, kecepatan_normal, kecepatan_cepat
+    timestep = 16
+
+    # Deklarasi Parameter
+    motor_pos_val = [0, 0]
+    sensor_ir_val = [0, 0, 0, 0, 0, 0]
+    sensor_jarak_val = [0, 0, 0]
+    
     # Deklarasi Motor
     motor = []
     motor_nama =['motor_1', 'motor_2']
@@ -56,15 +50,7 @@ def run_robot(robot):
     # Deklarasi Camera
     camera = robot.getDevice('CAM')
     camera.enable(timestep)
-
-    # Deklarasi Parameter
-    motor_pos_val = [0, 0]
-    sensor_ir_val = [0, 0, 0, 0, 0, 0]
-    sensor_jarak_val = [0, 0, 0]
     
-    #pid_parameter = [0.35, 0.00001, 2.2] #Kp Ki Kd
-    #pid_control = 0
-
     # Konversi nilai Ir sensor ke biner
     def read_ir():
         for i in range(6):
@@ -74,7 +60,7 @@ def run_robot(robot):
                 sensor_ir_val[i] = 0
 
     # Deklarasi posisi robot
-    def linefollow_pid():
+    def robot_pos():
         global nilai_posisi
         if sensor_ir_val == [1, 0, 0, 0, 0, 0] :
             nilai_posisi = 7
@@ -105,8 +91,34 @@ def run_robot(robot):
         elif sensor_ir_val == [0, 0, 0, 1, 1, 1] :
             nilai_posisi = -6
         elif sensor_ir_val == [0, 0, 0, 0, 0, 1] :
-            nilai_posisi = -7       
+            nilai_posisi = -7      
 
+    def kalkulasi_pid():
+        global pid_control
+        pid_parameter = [43, 0.2, 145] #Kp Ki Kd
+        error = [0, 0, 0]
+        control = [0, 0, 0]
+        set_point = 0
+
+        # Kendali Proporsional (P)
+        error[0] = set_point - nilai_posisi
+        control[0] = error[0] * pid_parameter[0]
+
+        # Kendali Integral (I)
+        error[1] = error[1] + error[0]
+        if error[1] > 7:
+            error[1] = 7
+        if error[1] <= -8:
+            error[1] = -7
+        control[1] = error[1] * pid_parameter[1]
+
+        # Kendali Differensial (D)
+        control[2] = (error[0] - error[2]) * pid_parameter[2]   
+        error[2] = error[0]
+
+        pid_control = control[0] + control[1] + control[2]
+        pid_control = kalkulasi_motor(pid_control)
+        
     # Main Loop
     while robot.step(timestep) != -1:
         for i in range(2):
@@ -115,11 +127,33 @@ def run_robot(robot):
         for i in range(6):
             sensor_ir_val[i] = sensor_ir[i].getValue()
         read_ir()
-        linefollow_pid()
-        #pid_calc()
+        robot_pos()
 
         for i in range(3):
             sensor_jarak_val[i] = sensor_jarak[i].getValue()
+
+        kalkulasi_pid()
+        kecepatan = kalkulasi_motor(kecepatan_normal)
+        kiri = kecepatan + pid_control
+        kanan = kecepatan - pid_control
+
+        if kiri > kecepatan_cepat :
+            kiri = kecepatan_cepat
+        if kiri < kecepatan_lambat :
+            kiri = kecepatan_lambat
+        if kanan > kecepatan_cepat :
+            kanan = kecepatan_cepat
+        if kanan < kecepatan_lambat :
+           kanan = kecepatan_lambat
+
+        if sensor_ir_val == [1, 1, 1, 1, 1, 0] or sensor_ir_val == [1, 1, 1, 1, 0, 0]:
+            kiri = 0
+            kanan = kecepatan_cepat
+        elif sensor_ir_val == [0, 1, 1, 1, 1, 1] or sensor_ir_val == [0, 0, 1, 1, 1, 1]:
+            kiri = kecepatan_cepat
+            kanan = 0
+
+        
 
         #if sensor_ir_val == [0, 0, 1, 1, 0, 0] :
             #motor[0].setVelocity(0)
@@ -131,14 +165,15 @@ def run_robot(robot):
 
         #print("IR sensor values: {:.3f} {:.3f} {:.3f} {:.3f} {:.3f} {:.3f}".format(sensor_ir_val[0], sensor_ir_val[1], sensor_ir_val[2], sensor_ir_val[3], sensor_ir_val[4], sensor_ir_val[5]))
         
-        print(nilai_posisi)
-        
+        print(kiri)
+        print(kanan)
+
         #print(*sensor_ir_val)
 
         #print("PD Control: {}".format(pid_control))
 
-        motor[0].setVelocity(0)
-        motor[1].setVelocity(0)
+        motor[0].setVelocity(kalkulasi_motor(kiri))
+        motor[1].setVelocity(kalkulasi_motor(kanan))
 
 if __name__ == "__main__":
     my_robot = Robot()
