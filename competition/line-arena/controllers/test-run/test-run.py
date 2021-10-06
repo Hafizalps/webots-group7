ca40b420c80f7d748fe49f78980d9ca024e57197
Hafizalps <hafizalperspicarhanggasidhi.2019@student.uny.ac.id>
2021-10-06 22:29:47 +0700

    Line follow (PID) + Wall follow (PD)

--- competition/line-arena/controllers/test-run/test-run.py
+++ competition/line-arena/controllers/test-run/test-run.py
@@ -4,14 +4,15 @@ def kalkulasi_motor(signal):
     return (signal/100)*30
 
 nilai_posisi = 0
-kecepatan_lambat = 20
-kecepatan_normal = 60
+kecepatan_lambat = 15 #15
+kecepatan_normal = 65 #65
 kecepatan_cepat = 100
-pid_control = 0
+pid_control_line = 0
+
 
 def run_robot(robot):
     global kecepatan_lambat, kecepatan_normal, kecepatan_cepat
-    timestep = 16
+    timestep = 8
 
     # Deklarasi Parameter
     motor_pos_val = [0, 0]
@@ -54,7 +55,7 @@ def run_robot(robot):
     # Konversi nilai Ir sensor ke biner
     def read_ir():
         for i in range(6):
-            if sensor_ir_val[i] < 600:
+            if sensor_ir_val[i] < 570:
                 sensor_ir_val[i] = 1
             else:
                 sensor_ir_val[i] = 0
@@ -63,15 +64,16 @@ def run_robot(robot):
     def robot_pos():
         global nilai_posisi
         if sensor_ir_val == [1, 0, 0, 0, 0, 0] :
-            nilai_posisi = 7
+            nilai_posisi = 10
         elif sensor_ir_val == [1, 1, 1, 0, 0, 0] :
-            nilai_posisi = 6
+            nilai_posisi = 15
         elif sensor_ir_val == [1, 1, 0, 0, 0, 0] :
             nilai_posisi = 5
         elif sensor_ir_val == [0, 1, 0, 0, 0, 0] :
             nilai_posisi = 4
         elif sensor_ir_val == [0, 1, 1, 1, 0, 0] :
             nilai_posisi = 3
+            print("Belok Kiri")
         elif sensor_ir_val == [0, 1, 1, 0, 0, 0] :
             nilai_posisi = 2
         elif sensor_ir_val == [0, 0, 1, 0, 0, 0] :
@@ -84,18 +86,29 @@ def run_robot(robot):
             nilai_posisi = -2
         elif sensor_ir_val == [0, 0, 1, 1, 1, 0] :
             nilai_posisi = -3
+            print("Belok Kanan")
         elif sensor_ir_val == [0, 0, 0, 0, 1, 0] :
             nilai_posisi = -4
         elif sensor_ir_val == [0, 0, 0, 0, 1, 1] :
             nilai_posisi = -5
         elif sensor_ir_val == [0, 0, 0, 1, 1, 1] :
-            nilai_posisi = -6
+            nilai_posisi = -15
         elif sensor_ir_val == [0, 0, 0, 0, 0, 1] :
-            nilai_posisi = -7      
+            nilai_posisi = -10
+        elif sensor_ir_val == [0, 0, 1, 1, 1, 1] : 
+            nilai_posisi = -30
+        elif sensor_ir_val == [0, 1, 1, 1, 1, 1] : 
+            nilai_posisi = -35
+            print("Belok Kanan Tajam")
+        elif sensor_ir_val == [1, 1, 1, 1, 0, 0] : 
+            nilai_posisi = 30
+        elif sensor_ir_val == [1, 1, 1, 1, 1, 0] : 
+            nilai_posisi = 35
+            print("Belok Kiri Tajam")
 
-    def kalkulasi_pid():
-        global pid_control
-        pid_parameter = [43, 0.2, 145] #Kp Ki Kd
+    def kalkulasi_pid_line():
+        global pid_control_line
+        pid_parameter = [20, 0.2, 70] #Kp Ki Kd 43, 0.2, 145 ; 20, 0.2, 70 ; 10, 0.2, 80
         error = [0, 0, 0]
         control = [0, 0, 0]
         set_point = 0
@@ -106,19 +119,27 @@ def run_robot(robot):
 
         # Kendali Integral (I)
         error[1] = error[1] + error[0]
-        if error[1] > 7:
-            error[1] = 7
-        if error[1] <= -8:
-            error[1] = -7
+        if error[1] > 35:
+            error[1] = 35
+        if error[1] <= -35:
+            error[1] = -35
         control[1] = error[1] * pid_parameter[1]
 
         # Kendali Differensial (D)
-        control[2] = (error[0] - error[2]) * pid_parameter[2]   
+        control[2] = (error[0] - error[2]) * pid_parameter[2]
         error[2] = error[0]
 
-        pid_control = control[0] + control[1] + control[2]
-        pid_control = kalkulasi_motor(pid_control)
-        
+        pid_control_line = control[0] + control[1] + control[2]
+        pid_control_line = kalkulasi_motor(pid_control_line)
+
+    # Parameter Untuk Kendali PD (Wall Follow)
+    pd_parameter = [0.5, 0.1] #Kp Kd
+    pd_control_wall = 0
+    leftPrevWall = 0
+    midPrevWall = 0
+    rightPrevWall = 0
+    distanceToWall = 990
+    
     # Main Loop
     while robot.step(timestep) != -1:
         for i in range(2):
@@ -132,11 +153,39 @@ def run_robot(robot):
         for i in range(3):
             sensor_jarak_val[i] = sensor_jarak[i].getValue()
 
-        kalkulasi_pid()
-        kecepatan = kalkulasi_motor(kecepatan_normal)
-        kiri = kecepatan + pid_control
-        kanan = kecepatan - pid_control
+        if sensor_jarak_val[0] < 990 or sensor_jarak_val[2] < 990 :
+            print("Algoritma Wall Following")
+            left_wall_error = sensor_jarak_val[0] - 780
+            right_wall_error = sensor_jarak_val[2] - 780
 
+            # Deteksi tembok kanan
+            if sensor_jarak_val[2] < distanceToWall :
+                pd_control_wall = pd_parameter[0] * right_wall_error + pd_parameter[1] * (right_wall_error - rightPrevWall)
+                rightPrevWall = right_wall_error
+                kiri = kecepatan_lambat + pd_control_wall
+                kanan = kecepatan_lambat - pd_control_wall
+                print("Telusur Tembok Kanan")
+            # Deteksi tembok kiri
+            elif sensor_jarak_val[0] < distanceToWall :
+                pd_control_wall = pd_parameter[0] * left_wall_error + pd_parameter[1] * (left_wall_error - leftPrevWall)
+                leftPrevWall = left_wall_error
+                kiri = kecepatan_lambat - pd_control_wall
+                kanan = kecepatan_lambat + pd_control_wall
+                print("Telusur Tembok Kiri")
+            # Robot diantara tembok kanan dan kiri (Tengah)
+            elif sensor_jarak_val[0] < distanceToWall and sensor_jarak_val[2] < distanceToWall :
+                pd_control_wall = pd_parameter[0] * (sensor_jarak_val[2] - sensor_jarak_val[0]) + pd_parameter[1] * (sensor_jarak_val[2] - sensor_jarak_val[0] - midPrevWall)
+                midPrevWall = sensor_jarak_val[2] - sensor_jarak_val[0]
+                kiri = kecepatan_lambat + pd_control_wall
+                kanan = kecepatan_lambat - pd_control_wall
+        else :
+            # Line follow
+            kalkulasi_pid_line()
+            kecepatan = kalkulasi_motor(kecepatan_normal)
+            kiri = kecepatan + pid_control_line
+            kanan = kecepatan - pid_control_line
+        
+        # Pembatas Kecepatan            
         if kiri > kecepatan_cepat :
             kiri = kecepatan_cepat
         if kiri < kecepatan_lambat :
@@ -144,33 +193,17 @@ def run_robot(robot):
         if kanan > kecepatan_cepat :
             kanan = kecepatan_cepat
         if kanan < kecepatan_lambat :
-           kanan = kecepatan_lambat
-
-        if sensor_ir_val == [1, 1, 1, 1, 1, 0] or sensor_ir_val == [1, 1, 1, 1, 0, 0]:
-            kiri = 0
-            kanan = kecepatan_cepat
-        elif sensor_ir_val == [0, 1, 1, 1, 1, 1] or sensor_ir_val == [0, 0, 1, 1, 1, 1]:
-            kiri = kecepatan_cepat
-            kanan = 0
-
-        
-
-        #if sensor_ir_val == [0, 0, 1, 1, 0, 0] :
-            #motor[0].setVelocity(0)
-            #motor[1].setVelocity(0)
+            kanan = kecepatan_lambat
             
         #print("Odometry sensor values: {:.3f} {:.3f}".format(motor_pos_val[0], motor_pos_val[1]))
-
         #print("Proximity sensor values: {:.3f} {:.3f} {:.3f}".format(sensor_jarak_val[0], sensor_jarak_val[1], sensor_jarak_val[2]))
-
         #print("IR sensor values: {:.3f} {:.3f} {:.3f} {:.3f} {:.3f} {:.3f}".format(sensor_ir_val[0], sensor_ir_val[1], sensor_ir_val[2], sensor_ir_val[3], sensor_ir_val[4], sensor_ir_val[5]))
         
-        print(kiri)
-        print(kanan)
+        #print(kiri)
+        #print(kanan)
 
         #print(*sensor_ir_val)
-
-        #print("PD Control: {}".format(pid_control))
+        #print("PID Control: {}".format(pid_control_line))
 
         motor[0].setVelocity(kalkulasi_motor(kiri))
         motor[1].setVelocity(kalkulasi_motor(kanan))
